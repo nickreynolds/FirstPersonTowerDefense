343b269cbaa7f84fc8b5f9720d7efc0790138cc7
Nick Reynolds <nicholas.s.reynolds@gmail.com>
2015-10-23 00:10:06 -0700

    adding new levels for Oculus and regular Mouse&Keyboard versions

---
+++ Assets/Scripts/OculusInputController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OculusInputController : MonoBehaviour 
+{
+
+	
+	public float MAX_TAP_LENGTH = 0.75f;
+	private float timeBeganTap;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			timeBeganTap = Time.time;
+		}
+		else if (Input.GetMouseButtonUp(0))
+		{
+			if(Time.time < (timeBeganTap + MAX_TAP_LENGTH))
+			{
+				Player.instance.placeTower();
+			}
+		}
+	}
+
+}
